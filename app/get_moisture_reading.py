3be096f7d99706a49eb47b8d73623d38f496ed2c
NicolasBuitrago <nicolasbuitragobautista@gmail.com>
2024-11-22 20:49:59 -0500

    fix

--- app/get_moisture_reading.py
+++ app/get_moisture_reading.py
@@ -28,7 +28,7 @@ def get_moisture_reading(raw_val):
 
 if __name__ == "__main__":
     channel = create_chanel()
-    value = get_moisture_reading(channel.value,1)
+    value = get_moisture_reading(channel.value)
     record = MoistureRecord(moisture_level=value,plant_id=1,created_at=datetime.now(),updated_at=datetime.now())
     create_moisture_record(session,record)
     
