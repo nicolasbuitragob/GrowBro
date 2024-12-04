35e22d7198c1f6a4365486cf1ec06ced3686a004
NicolasBuitrago <nicolasbuitragobautista@gmail.com>
2024-12-03 22:38:49 -0500

    add telegram bot

--- app/get_moisture_reading.py
+++ app/get_moisture_reading.py
@@ -9,10 +9,24 @@ from database.connection import get_session
 from database.models import MoistureRecord
 from create_records import create_moisture_record
 from datetime import datetime
+
+from aiogram.methods.send_message import SendMessage
+from os import getenv
 from moisture_sensor.create_chanel import create_chanel
+from aiogram import Bot, Dispatcher, html
 
+TOKEN = getenv("TELEGRAM_BOT_TOKEN")
+bot = Bot(token=TOKEN)
 session = get_session()
+dp = Dispatcher()
 
+@dp.message()   
+async def send_moisture_reading_alert(moisture_reading):
+    bot = Bot(token=TOKEN)
+    try:
+        await bot.send_message(chat_id=7364502823, text=f"hey something is wrong!!! the moisture level is {moisture_reading}")
+    finally:
+        await bot.session.close()
 
 def read_calibration_values():
     with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/sensor_calibration.json', 'r') as f:
@@ -20,16 +34,27 @@ def read_calibration_values():
 
 calibration_values = read_calibration_values()
 
-def get_moisture_reading(raw_val):
-    per_val = abs((raw_val- int(calibration_values["max_dry_value"]))/(int(calibration_values["min_wet_value"])-int(calibration_values["max_dry_value"])))*100
-    return round(per_val, 3)
-    
+async def get_moisture_reading(raw_val):
+    moisture_reading = abs((raw_val- int(calibration_values["max_dry_value"]))/(int(calibration_values["min_wet_value"])-int(calibration_values["max_dry_value"])))*100
+    if moisture_reading > 100 or moisture_reading < 15:
+        await send_moisture_reading_alert(moisture_reading)
+    return round(moisture_reading, 3)
+
+
 
 
 if __name__ == "__main__":
-    channel = create_chanel()
-    value = get_moisture_reading(channel.value)
-    record = MoistureRecord(moisture_level=value,plant_id=1,created_at=datetime.now(),updated_at=datetime.now())
-    create_moisture_record(session,record)
+    import asyncio
     
+    async def main():
+        try:
+            channel = create_chanel()
+            value = get_moisture_reading(channel.value)
+            record = MoistureRecord(moisture_level=value,plant_id=1,created_at=datetime.now(),updated_at=datetime.now())
+            create_moisture_record(session,record)
 
+        finally:
+            await bot.session.close()
+    
+    asyncio.run(main())
+    
--- app/main.py
+++ app/main.py
@@ -8,9 +8,32 @@ from database.models import MoistureRecord
 from typing import List
 from fastapi.responses import HTMLResponse
 from views.moisture import get_moisture_view,get_moisture_table
+import asyncio
+import logging
+import sys
+from os import getenv
 
-app = FastAPI()
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
+import uvicorn
+from contextlib import asynccontextmanager
+
+
+TOKEN = getenv("TELEGRAM_BOT_TOKEN")
+dp = Dispatcher()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    asyncio.create_task(dp.start_polling(bot))
+    yield
+    await bot.session.close()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
@@ -25,3 +48,28 @@ def moisture_view(request: Request,plant_id: int):
 @app.get("/plants/{plant_id}/moisture-table")
 def moisture_table(request: Request,plant_id: int):
     return get_moisture_table(request,plant_id)
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    session = get_session()
+    query = select(MoistureRecord.moisture_level,
+                   MoistureRecord.created_at,
+                   MoistureRecord.plant_id).order_by(MoistureRecord.created_at.desc()).limit(1)
+    
+    result = session.exec(query).first()
+    
+    if result:
+        moisture_level = result.moisture_level
+        created_at = result.created_at
+        created_at_formatted = created_at.strftime("%Y-%m-%d %H:%M:%S")
+        await message.answer(f"Hello! The moisture percentage for your plant is {moisture_level} and the last record was taken at {created_at_formatted}")
+    else:
+        await message.answer("No moisture records found!")
+    
+    session.close()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
